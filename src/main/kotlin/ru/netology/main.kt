11f516d4a31ef8ca4de0489fc5d207c2e5772b99
Elena Savina <savina1289@yandex.ru>
2022-06-22 03:07:21 +0300

    Added block if-else

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,2 +1,13 @@
 package ru.netology
 
+fun main() {
+    val likes = 4523
+
+    if ((likes % 1000) == 0) {
+        println("Понравилось $likes людей")
+    } else if ((likes % 10) == 1 && (likes % 100) != 11) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
+}
