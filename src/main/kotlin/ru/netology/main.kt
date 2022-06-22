eb84fb2b403d0c15c22dbc4d858a019e26be0eea
Elena Savina <savina1289@yandex.ru>
2022-06-22 03:54:41 +0300

    Changed expression if-else

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,13 +1,13 @@
 package ru.netology
 
 fun main() {
-    val likes = 4523
+    val likes = 111
 
-    if ((likes % 1000) == 0) {
-        println("Понравилось $likes людей")
-    } else if ((likes % 10) == 1 && (likes % 100) != 11) {
-        println("Понравилось $likes человеку")
-    } else {
-        println("Понравилось $likes людям")
-    }
+    val endPhrase =
+        if ((likes % 1000) == 0) "людей"
+        else if ((likes % 10) == 1 && (likes % 100) != 11) "человеку"
+        else "людям"
+
+    println("Понравилось $likes $endPhrase")
+
 }
